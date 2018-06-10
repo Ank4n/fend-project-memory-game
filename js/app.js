672a68b7ca820a2d1ac92fed99e846234ee01042
Ankan <ankan.anurag@gmail.com>
2018-06-11 02:11:58 +0530

    Added code for interactive memory game

--- js/app.js
+++ js/app.js
@@ -1,18 +1,35 @@
-/*
- * Create a list that holds all of your cards
- */
+let cards = [
+    "fa-diamond",
+    "fa-paper-plane-o",
+    "fa-anchor",
+    "fa-bolt",
+    "fa-cube",
+    "fa-anchor",
+    "fa-leaf",
+    "fa-bicycle",
+    "fa-diamond",
+    "fa-bomb",
+    "fa-leaf",
+    "fa-bomb",
+    "fa-bolt",
+    "fa-bicycle",
+    "fa-paper-plane-o",
+    "fa-cube"
+];
 
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+// Game state variables
+let firstCard,
+    firstCardIcon,
+    timerView,
+    timer,
+    totalTime,
+    moves,
+    firstCardSelected,
+    matchCount;
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,14 +42,135 @@ function shuffle(array) {
     return array;
 }
 
+// Create each card's html, closed initially
+function createDeckFrom(cards) {
+    for (let i = 0; i < cards.length; i++) {
+        $("#cards").append("<li class='card'><i class='fa " + cards[i] + "'></i></li>");
+    }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+}
+
+// update user moves and stars
+function updateScore() {
+    // Update moves
+    $(".moves").text(moves);
+
+    let all_stars = $(".stars");
+    if ((moves > 12) && (moves <= 16)) {
+        all_stars.children().remove("li");
+        all_stars.append("<li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li><li><i class='fa fa-star-o'></i></li>"); // Two stars
+
+    } else if (moves > 17) {
+        all_stars.children().remove("li");
+        all_stars.append("<li><i class='fa fa-star'></i></li><li><i class='fa fa-star-o'></i></li><li><i class='fa fa-star-o'></i></li>"); // A star
+    }
+}
+
+// action when user clicks on replay button
+function setReplayAction() {
+    $(".fa-repeat").click(function () {
+        $("ul#cards").children().remove("li");
+        $(".stars").children().remove("li");
+        $(".stars").append("<li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li><li><i class='fa fa-star'></i></li>"); // RESET stars
+        $(".moves").text("0");
+        clearInterval(timerView);
+        $("#timer").text("0.000");
+        init();
+    });
+}
+
+function match(firstCard) {
+    $(this).addClass("match");
+    firstCard.removeClass("open show").addClass("match");
+    firstCardSelected = false;
+    matchCount++;
+}
+
+function noMatch() {
+    $(this).addClass("open show");
+    $(".card").addClass("disabled");
+    setTimeout(function () {
+        $(".open.show").removeClass("open show");
+        $(".card").removeClass("disabled");
+    }, 800);
+    firstCardSelected = false;
+}
+
+function gameOver() {
+    console.log(totalTime);
+    $("#timer").text(totalTime);
+    clearInterval(timerView);
+
+    setTimeout(function () {
+        alert("Congratulations! You've won the game!");
+    }, 500);
+}
+
+function setTimer() {
+    if (timer === 1) {
+        let startTime = Date.now();
+        timerView = setInterval(function () {
+            let elapsedTime = Date.now() - startTime;
+            totalTime = (elapsedTime / 1000).toFixed(3);
+            $("#timer").text(totalTime);
+        }, 44);
+
+    }
+}
+
+function init() {
+    firstCard = '';
+    firstCardIcon = '';
+    timerView = null;
+    timer = 0;
+    totalTime = 0;
+    moves = 0;
+    firstCardSelected = false;
+    matchCount = 0;
+
+    cards = shuffle(cards);
+    createDeckFrom(cards);
+
+    $(".card").on("click", function () {
+
+        //game begins
+        let currentCard = $(this).attr("class");
+        let currentCardIcon = $(this).children().attr("class");
+
+        if (currentCard !== "card") return;
+
+        timer++;
+
+        // Select first card
+        if (!firstCardSelected) {
+            $(this).addClass("open show");
+            firstCardIcon = currentCardIcon;
+            firstCard = $(this);
+            firstCardSelected = true;
+        }
+
+        // when first card matches the second card
+        else if (currentCardIcon === firstCardIcon) {
+            match.call(this, firstCard);
+            moves++;
+        }
+
+        // when second card selected is different from first card
+        else {
+            noMatch.call(this);
+            moves++;
+        }
+
+        updateScore();
+
+        // check if all cards are matched
+        if (matchCount === 8)
+            gameOver();
+
+        setTimer();
+        setReplayAction();
+
+    });
+}
+
+init();
